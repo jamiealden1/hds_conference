3d9ab701dde6109a4a7430d336d2b4a197f32d1c
Jamie Alden <jamielynn_21@Macintosh.local>
2017-01-29 22:01:25 -0700

    Fixed Navigation

--- js/hds.js
+++ js/hds.js
@@ -12,4 +12,14 @@ $(document).ready(function() {
 		$(".d2").css({"color":"#999999", "border-bottom":"0px"});
 		$(".d1").css({"color":"#009fc2", "border-bottom":"2px solid #009fc2"});
 	});
-});
+
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 100;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
+	});
