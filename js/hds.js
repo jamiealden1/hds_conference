4504ad1a978b4006043c5713bf47d13d93e410e5
Jamie Alden <jamielynn_21@Macintosh.local>
2017-01-29 22:14:50 -0700

    Scroll to anchors

--- js/hds.js
+++ js/hds.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	//Toggle schedule days
 	$(".d2").click(function() {
 		$("#day1").fadeOut(500);
 		$("#day2").fadeIn(1000);
@@ -13,13 +15,29 @@ $(document).ready(function() {
 		$(".d1").css({"color":"#009fc2", "border-bottom":"2px solid #009fc2"});
 	});
 
-	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 100;
+	//Scroll then fixed navigation
+	$(window).bind('scroll', function() {
+		var navHeight = $( window ).height() - 100;
 			 if ($(window).scrollTop() > navHeight) {
 				 $('nav').addClass('fixed');
 			 }
 			 else {
 				 $('nav').removeClass('fixed');
 			 }
-		});
 	});
+
+	//Scroll to anchor points
+	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+      
+        var target = this.hash,
+        $target = $(target);
+      
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-100
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+});
